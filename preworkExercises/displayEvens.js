28403d6e0e3b0f977021b20da38ed67ac00976a4
Teresa Horton <teresal.horton@libertymutual.com>
2018-01-18 12:05:28 -0500

    display evens is working!

--- preworkExercises/displayEvens.js
+++ preworkExercises/displayEvens.js
@@ -1,7 +1,49 @@
+
+/*maybe place verify function here and if it's true/false, have it call the evenNumbers function or display warning and reset fields*/
+function verify()
+{
+	var y=document.getElementById("evens");
+	var startNum=parseInt(y.elements["startingNumber"].value);
+	var endNum=parseInt(y.elements["endingNumber"].value);
+	var stepNum= parseInt(y.elements["step"].value);
+	var msg = "";
+	
+	if (stepNum <0)
+	{
+		msg+="Please enter a positive number value! \n";
+		stepNum.className = "inpBoxError";
+	}
+	
+	if (endNum <= startNum)
+	{
+		msg+="Please enter a number greater than the starting Number! \n"
+		endNum.className = "inpBoxError";
+	}
+	
+	if (msg=="")
+	{
+	return true;
+	} else {
+	alert(msg);
+	return false;
+	}
+	
+}
+
+
+
+
 function evenNumbers (start, end, step)
 {
 var beginArray=new Array();
 var evenArray= new Array();
+var x=document.getElementById("evens");
+var starting=parseInt(x.elements["startingNumber"].value);
+var end=parseInt(x.elements["endingNumber"].value);
+var step=  parseInt(x.elements["step"].value);
+	
+	
+		verify();
 
 	
 		for (var i=start; i<=end; i++)
@@ -18,6 +60,9 @@ var evenArray= new Array();
 			
 			}
 		}
-	return (evenArray);
+		
+		var answer=document.write ("Here are the even numbers between " + starting +" and " + end + " by " + step + "s: \n" + evenArray[] );
+		
+	return (answer);
 	
 }
